7938e14605f23fb0af2d3a353d70de66d3a98f08
Sean DuBois <sean@siobud.com>
2024-07-16 13:28:41 -0400

    Move wifi to dedicated file

--- main/main.c
+++ main/main.c
@@ -1,51 +1,6 @@
-#include <assert.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "esp_event.h"
-#include "esp_log.h"
-#include "esp_wifi.h"
 #include "nvs_flash.h"
 
-static const char *LOG_TAG = "embedded-sdk";
-
-static void init_wifi(void) {
-  ESP_ERROR_CHECK(esp_netif_init());
-  esp_netif_t *sta_netif = esp_netif_create_default_wifi_sta();
-  assert(sta_netif);
-
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_start());
-
-  wifi_config_t wifi_config;
-  memset(&wifi_config, 0, sizeof(wifi_config));
-  strncpy((char *)wifi_config.sta.ssid, (char *)WIFI_SSID,
-          sizeof(wifi_config.sta.ssid));
-  strncpy((char *)wifi_config.sta.password, (char *)WIFI_PASSWORD,
-          sizeof(wifi_config.sta.password));
-
-  ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-  ESP_ERROR_CHECK(esp_wifi_connect());
-}
-
-static void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data) {
-  static int s_retry_num = 0;
-  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
-    if (s_retry_num < 5) {
-      esp_wifi_connect();
-      s_retry_num++;
-      ESP_LOGI(LOG_TAG, "retry to connect to the AP");
-    }
-    ESP_LOGI(LOG_TAG, "connect to the AP fail");
-  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-    ESP_LOGI(LOG_TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-  }
-}
+extern void app_wifi(void);
 
 void app_main(void) {
   esp_err_t ret = nvs_flash_init();
@@ -56,10 +11,5 @@ void app_main(void) {
   }
   ESP_ERROR_CHECK(ret);
 
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
-  ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID,
-                                             &event_handler, NULL));
-  ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP,
-                                             &event_handler, NULL));
-  init_wifi();
+  app_wifi();
 }

73fce666987e0dff7d57de8ffd2d9d6d923ec731
Sean DuBois <sean@siobud.com>
2024-08-06 16:12:43 -0400

    Minimize app_main

--- main/main.cpp
+++ main/main.cpp
@@ -3,11 +3,7 @@
 extern void app_wifi(void);
 extern void app_websocket(void);
 
-extern "C" {
-extern void app_main(void);
-}
-
-void app_main(void) {
+extern "C" void app_main(void) {
   esp_err_t ret = nvs_flash_init();
   if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
       ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {

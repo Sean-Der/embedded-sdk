40f50852f53b955eb729d4deea68aa860ea68f93
Stephan Rotolante <stephanrotolante@gmail.com>
2024-08-15 00:16:45 -0400

    Implement WebSocket read + unpacking in protobuf

--- src/websocket.cpp
+++ src/websocket.cpp
@@ -1,9 +1,18 @@
+#include <vector>
+
 #include "esp_event.h"
 #include "esp_log.h"
 #include "esp_websocket_client.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+#include "livekit_rtc.pb-c.h"
 
 static const char *LOG_TAG = "embedded-sdk";
 #define WEBSOCKET_URI_SIZE 1500
+#define MTU_SIZE 1500
+
+std::vector<Livekit__SignalRequest *> app_websocket_packets;
+SemaphoreHandle_t app_websocket_mutex;
 
 static void app_websocket_event_handler(void *handler_args,
                                         esp_event_base_t base, int32_t event_id,
@@ -18,13 +27,21 @@ static void app_websocket_event_handler(void *handler_args,
       break;
     case WEBSOCKET_EVENT_DATA:
       ESP_LOGI(LOG_TAG, "WEBSOCKET_EVENT_DATA");
-      ESP_LOGI(LOG_TAG, "Received opcode=%d", data->op_code);
       if (data->op_code == 0x08 && data->data_len == 2) {
-        ESP_LOGW(LOG_TAG, "Received closed message with code=%d",
-                 256 * data->data_ptr[0] + data->data_ptr[1]);
-      } else {
-        ESP_LOGW(LOG_TAG, "Received=%.*s", data->data_len,
-                 (char *)data->data_ptr);
+        return;
+      }
+
+      if (xSemaphoreTake(app_websocket_mutex, portMAX_DELAY) == pdTRUE) {
+        auto new_request = livekit__signal_request__unpack(
+            NULL, data->data_len, (uint8_t *)data->data_ptr);
+
+        if (new_request == NULL) {
+          ESP_LOGE(LOG_TAG, "Failed to decode SignalRequest message.\n");
+        } else {
+          app_websocket_packets.push_back(new_request);
+        }
+
+        xSemaphoreGive(app_websocket_mutex);
       }
       break;
     case WEBSOCKET_EVENT_ERROR:
@@ -34,6 +51,12 @@ static void app_websocket_event_handler(void *handler_args,
 }
 
 void app_websocket(void) {
+  app_websocket_mutex = xSemaphoreCreateMutex();
+  if (app_websocket_mutex == NULL) {
+    ESP_LOGE(LOG_TAG, "Failed to create mutex.\n");
+    return;
+  }
+
   char ws_uri[WEBSOCKET_URI_SIZE];
   snprintf(ws_uri, sizeof(ws_uri), "%s/rtc?access_token=%s", LIVEKIT_URL,
            LIVEKIT_TOKEN);
@@ -42,6 +65,8 @@ void app_websocket(void) {
   memset(&ws_cfg, 0, sizeof(ws_cfg));
 
   ws_cfg.uri = ws_uri;
+  ws_cfg.buffer_size = MTU_SIZE;
+  ws_cfg.disable_pingpong_discon = true;
 
   auto client = esp_websocket_client_init(&ws_cfg);
   esp_websocket_register_events(client, WEBSOCKET_EVENT_ANY,
@@ -49,9 +74,63 @@ void app_websocket(void) {
 
   esp_websocket_client_start(client);
 
-  // TODO. State machine
+  // Example sending data
+  // Livekit__AddTrackRequest r LIVEKIT__ADD_TRACK_REQUEST__INIT;
+  // size_t size = livekit__add_track_request__get_packed_size(&r);
+  // uint8_t *buffer =(uint8_t*) malloc(size);
+  // livekit__add_track_request__pack(&r,buffer);
+  // esp_err_t err = esp_websocket_client_send_bin(client,(char*)buffer,size,
+  // portMAX_DELAY); if (err != ESP_OK) {
+  //   printf("Failed to send data: %s\n", esp_err_to_name(err));
+  // } else {
+  //   printf("Data sent successfully\n");
+  // }
+
   while (true) {
-    vTaskDelay(pdMS_TO_TICKS(5000));
+    if (xSemaphoreTake(app_websocket_mutex, portMAX_DELAY) == pdTRUE) {
+      if (!app_websocket_packets.empty()) {
+        auto packet = app_websocket_packets.front();
+
+        switch (packet->message_case) {
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE__NOT_SET:
+            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_REQUEST__MESSAGE__NOT_SET\n");
+            break;
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE_OFFER:
+            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_REQUEST__MESSAGE_OFFER\n");
+            break;
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE_ANSWER:
+            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_REQUEST__MESSAGE_ANSWER\n");
+            break;
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE_TRICKLE:
+            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_REQUEST__MESSAGE_TRICKLE\n");
+            break;
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE_ADD_TRACK:
+            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_REQUEST__MESSAGE_ADD_TRACK\n");
+            break;
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE_MUTE:
+            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_REQUEST__MESSAGE_MUTE\n");
+            break;
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE_SUBSCRIPTION:
+            ESP_LOGI(LOG_TAG,
+                     "LIVEKIT__SIGNAL_REQUEST__MESSAGE_SUBSCRIPTION\n");
+            break;
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE_TRACK_SETTING:
+            ESP_LOGI(LOG_TAG,
+                     "LIVEKIT__SIGNAL_REQUEST__MESSAGE_TRACK_SETTING\n");
+            break;
+          case LIVEKIT__SIGNAL_REQUEST__MESSAGE_LEAVE:
+            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_REQUEST__MESSAGE_LEAVE\n");
+            break;
+          default:
+            ESP_LOGI(LOG_TAG, "Unknown message type received.\n");
+            break;
+        }
+        app_websocket_packets.erase(app_websocket_packets.begin());
+        livekit__signal_request__free_unpacked(packet, NULL);
+      }
+      xSemaphoreGive(app_websocket_mutex);
+    }
+    vTaskDelay(pdMS_TO_TICKS(200));
   }
 }
 

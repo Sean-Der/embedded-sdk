309ded13e8debb9eef3bd3e1d53cc0f330be1a48
Sean DuBois <sean@siobud.com>
2024-09-11 23:04:18 -0400

    PeerConnection goes to checking
    Answer is not sent to LiveKit yet

--- src/websocket.cpp
+++ src/websocket.cpp
@@ -18,23 +18,33 @@
 
 static const char *LOG_TAG = "embedded-sdk";
 
-#define WEBSOCKET_URI_SIZE 400
+#define STRING_BUFFER_SIZE 750
 #define MTU_SIZE 1500
 #define LIVEKIT_PROTOCOL_VERSION 3
 
 std::vector<Livekit__SignalResponse *> app_websocket_packets;
-SemaphoreHandle_t app_websocket_mutex;
+SemaphoreHandle_t g_mutex;
 
 static void *peer_connection_task(void *user_data) {
   PeerConnection *peer_connection = (PeerConnection *)user_data;
 
   while (1) {
     peer_connection_loop(peer_connection);
-    vTaskDelay(pdMS_TO_TICKS(1));
+    vTaskDelay(pdMS_TO_TICKS(20));
   }
 
   pthread_exit(NULL);
   return NULL;
+}
+
+static void on_icecandidate_task(char *description, void *user_data) {
+  printf("%s \n", description);
+}
+
+static void onconnectionstatechange_task(PeerConnectionState state,
+                                         void *user_data) {
+  ESP_LOGI(LOG_TAG, "PeerConnectionState: %s",
+           peer_connection_state_to_string(state));
 }
 
 static void app_websocket_event_handler(void *handler_args,
@@ -53,7 +63,7 @@ static void app_websocket_event_handler(void *handler_args,
         return;
       }
 
-      if (xSemaphoreTake(app_websocket_mutex, portMAX_DELAY) == pdTRUE) {
+      if (xSemaphoreTake(g_mutex, portMAX_DELAY) == pdTRUE) {
         auto new_response = livekit__signal_response__unpack(
             NULL, data->data_len, (uint8_t *)data->data_ptr);
 
@@ -63,7 +73,7 @@ static void app_websocket_event_handler(void *handler_args,
           app_websocket_packets.push_back(new_response);
         }
 
-        xSemaphoreGive(app_websocket_mutex);
+        xSemaphoreGive(g_mutex);
       }
       break;
     case WEBSOCKET_EVENT_ERROR:
@@ -73,33 +83,42 @@ static void app_websocket_event_handler(void *handler_args,
 }
 
 void app_websocket(void) {
-  app_websocket_mutex = xSemaphoreCreateMutex();
-  if (app_websocket_mutex == NULL) {
+  g_mutex = xSemaphoreCreateMutex();
+  if (g_mutex == NULL) {
     ESP_LOGE(LOG_TAG, "Failed to create mutex.\n");
     return;
   }
 
-  char ws_uri[WEBSOCKET_URI_SIZE];
-  snprintf(ws_uri, sizeof(ws_uri),
+  char *string_buffer = (char *)calloc(1, STRING_BUFFER_SIZE);
+  snprintf(string_buffer, STRING_BUFFER_SIZE,
            "%s/rtc?protocol=%d&access_token=%s&auto_subscribe=true",
            LIVEKIT_URL, LIVEKIT_PROTOCOL_VERSION, LIVEKIT_TOKEN);
 
   esp_websocket_client_config_t ws_cfg;
   memset(&ws_cfg, 0, sizeof(ws_cfg));
 
-  ws_cfg.uri = ws_uri;
+  ws_cfg.uri = string_buffer;
   ws_cfg.buffer_size = MTU_SIZE;
   ws_cfg.disable_pingpong_discon = true;
+  ws_cfg.reconnect_timeout_ms = 1000;
+  ws_cfg.network_timeout_ms = 1000;
 
   auto client = esp_websocket_client_init(&ws_cfg);
   esp_websocket_register_events(client, WEBSOCKET_EVENT_ANY,
                                 app_websocket_event_handler, (void *)client);
-
   esp_websocket_client_start(client);
 
   PeerConfiguration peer_connection_config = {
-      .audio_codec = CODEC_OPUS,
-      .video_codec = CODEC_H264,
+      .ice_servers = {
+          {.urls = "stun:stun.l.google.com:19302"},
+      },
+      .audio_codec = CODEC_NONE,
+      .video_codec = CODEC_NONE,
+      .datachannel = DATA_CHANNEL_STRING,
+      .onaudiotrack = NULL,
+      .onvideotrack = NULL,
+      .on_request_keyframe = NULL,
+      .user_data = NULL,
   };
 
   peer_init();
@@ -107,11 +126,18 @@ void app_websocket(void) {
   PeerConnection *subscriber_peer_connection =
       peer_connection_create(&peer_connection_config);
 
+  peer_connection_onicecandidate(subscriber_peer_connection,
+                                 on_icecandidate_task);
+  peer_connection_oniceconnectionstatechange(subscriber_peer_connection,
+                                             onconnectionstatechange_task);
+
   pthread_t subscriber_peer_connection_thread_handle;
-  pthread_create(&subscriber_peer_connection_thread_handle, NULL, peer_connection_task, subscriber_peer_connection);
+  pthread_create(&subscriber_peer_connection_thread_handle, NULL,
+                 peer_connection_task, subscriber_peer_connection);
+  memset(string_buffer, 0, STRING_BUFFER_SIZE);
 
   while (true) {
-    if (xSemaphoreTake(app_websocket_mutex, portMAX_DELAY) == pdTRUE) {
+    if (xSemaphoreTake(g_mutex, portMAX_DELAY) == pdTRUE) {
       if (!app_websocket_packets.empty()) {
         auto packet = app_websocket_packets.front();
 
@@ -127,10 +153,14 @@ void app_websocket(void) {
             break;
           case LIVEKIT__SIGNAL_RESPONSE__MESSAGE_OFFER:
             ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_RESPONSE__MESSAGE_OFFER\n");
+            strncpy(string_buffer, packet->offer->sdp, STRING_BUFFER_SIZE);
             break;
           case LIVEKIT__SIGNAL_RESPONSE__MESSAGE_TRICKLE:
-            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_RESPONSE__MESSAGE_TRICKLE %s \n",
-                     packet->trickle->candidateinit);
+            ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_RESPONSE__MESSAGE_TRICKLE\n");
+            peer_connection_add_ice_candidate(subscriber_peer_connection,
+                                              packet->trickle->candidateinit);
+            peer_connection_set_remote_description(subscriber_peer_connection,
+                                                   string_buffer);
             break;
           case LIVEKIT__SIGNAL_RESPONSE__MESSAGE_UPDATE:
             ESP_LOGI(LOG_TAG, "LIVEKIT__SIGNAL_RESPONSE__MESSAGE_UPDATE\n");
@@ -159,7 +189,7 @@ void app_websocket(void) {
         app_websocket_packets.erase(app_websocket_packets.begin());
         livekit__signal_response__free_unpacked(packet, NULL);
       }
-      xSemaphoreGive(app_websocket_mutex);
+      xSemaphoreGive(g_mutex);
     }
     vTaskDelay(pdMS_TO_TICKS(200));
   }
